2eb7d03d95c3b0d86f2be71cd8a015cf9130c94a
nattybe <themanintheomoon@gmail.com>
2023-07-28 13:35:15 +0300

    messaging done kinda

--- src/Comp/contact.js
+++ src/Comp/contact.js
@@ -1,18 +1,24 @@
-// import { SMTPClient } from "emailjs";
-// import 'sendmail/sendmail'
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
-
-import { initializeApp } from "firebase/app";
-import firebase from "firebase/app";
-import { getAuth } from "firebase/auth";
-import "firebase/firestore";
-
-// import  firestore  from "firebase/firestore";
-// import { Auth } from "firebase/auth";
-
-import { useCollectionData } from "react-firebase-hooks/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  AuthPage,
+  auth,
+  firestore,
+  signOuter,
+} from "../firebase-comps/firebase-exp";
+import { useState } from "react";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+  query,
+  where,
+  orderBy,
+  or,
+} from "firebase/firestore";
+import { useEffect } from "react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD7Pds7gKW7V0cv3rrWYwUgDL0xoso-5Uc",
@@ -23,13 +29,41 @@ const firebaseConfig = {
   appId: "1:160274144232:web:99b684b0fb85cc1a38625d",
 };
 
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-// const firestore = firebase.firestore();
-
+// const messageTemplate = {
+//   from,to,content,createdAt,
+// };
 export default function Contacts() {
+  const [messages, setMessages] = useState([]);
   const [user] = useAuthState(auth);
+  const signOut = () => {
+    signOuter();
+    setMessages([]);
+    // alert("Signed out");
+  };
+  const messagesCollectionRef = collection(firestore, "messages");
+  const getMessages = async () => {
+    const q = query(
+      messagesCollectionRef,
+      (or(where("from", "==", user.uid), where("to", "==", user.uid)),
+      orderBy("createdAt", "asc"))
+    );
+    const data = await getDocs(q).catch((err) => {
+      // alert(err.code,err.line);
+      console.warn(err);
+    });
+    // const data = await getDocs(messagesCollectionRef).catch((err) => {
+    //   alert(err.code);
+    // }); //.then(() =>{
+    //   alert("Success");
+    // })
+    console.log(data);
+    const filteredData = data.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    setMessages(filteredData);
+    console.log(filteredData);
+  };
   const oldContacts = (
     <>
       <h1 className="text-center">Contact Me</h1>
@@ -79,82 +113,101 @@ export default function Contacts() {
       </div>
     </>
   );
-  const signin = () => {
-    // const pr=getAuth.
-    /* TODO: lost in the abyss */
-  };
-  // const client = new SMTPClient({
-  //   user: "user",
-  //   password: "password",
-  //   host: "smtp.your-email.com",
-  //   ssl: true,
-  // });
-  // const messanger = async () => {
-  //   const message = await client.sendAsync({
-  //     text: "hello world",
-  //     from: "you <username@your-email.com>",
-  //     to: "themanintheomoon@gmail.com",
-  //     cc: "else <else@your-email.com>",
-  //     subject: "testing emailjs",
-  //   });
-  // };
-  // const sendmail = require("sendmail")();
 
-  // sendmail(
-  //   {
-  //     from: "no-reply@yourdomain.com",
-  //     to: "test@qq.com, test@sohu.com, test@163.com ",
-  //     subject: "test sendmail",
-  //     html: "Mail of test sendmail ",
-  //   },
-  //   function (err, reply) {
-  //     console.log(err && err.stack);
-  //     console.dir(reply);
-  //   }
-  // );
+  useEffect(() => {
+    if (user) {
+      getMessages();
+    }
+  }, []);
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const theMessage = document.getElementById("theMessage");
+    // alert(theMessage.value);
+    await addDoc(messagesCollectionRef, {
+      content: theMessage.value,
+      from: user.uid,
+      to: "owner",
+      createdAt: serverTimestamp(),
+    })
+      .then(() => {
+        // alert("success");
+        getMessages();
+      })
+      .catch((err) => {
+        alert(err.code);
+      });
+    theMessage.value = "";
+  };
+  const messageFiller = () => {
+    const tempMessages = [];
+    if (messages) {
+      messages.map((mes) => {
+        if (mes.from === "owner") {
+          tempMessages.push(<div className="owner">{mes.content}</div>);
+        } else {
+          tempMessages.push(<div className="not-owner">{mes.content}</div>);
+        }
+      });
+      return tempMessages;
+    } else {
+      return <div className="muted h1">Say Something</div>;
+    }
+  };
   const messenger = (
-    <dialog id="contact-diag" className="border border-warning p-2 rounded-2">
-      <div className="messenger">
-        <div className="diag-header">
-          <h3> Contact me</h3>
-          <button
-            className="diag-close"
-            onClick={() => {
-              document.getElementById("contact-diag").close();
-            }}
-          >
-            X
-          </button>
+    <div className="messenger">
+      <div className="diag-header">
+        {user ? <h6>{user.email}</h6> : <h3>Contact me</h3>}
+        <button
+          className="btn btn-primary p-1 "
+          onClick={() => {
+            signOut();
+          }}
+        >
+          sign Out
+        </button>
+        <button
+          className="diag-close"
+          onClick={() => {
+            document.getElementById("contact-diag").close();
+          }}
+        >
+          X
+        </button>
+      </div>
+      <div className="messenger-body">
+        <div className="messages d-flex">
+          {messageFiller()}
+          {/* <div className="not-owner">hello am contacting</div>
+          <div className="not-owner">hello am contacting</div>
+          <div className="not-owner">hello am contacting</div>
+          <div className="not-owner">hello am contacting</div>
+          <div className="not-owner">hello am contacting</div>
+          <div className="not-owner">hello am contacting</div>
+          <div className="not-owner">hello am contacting</div>
+          <div className="not-owner">hello am contacting</div>
+          <div className="owner">and am the owner.</div>
+          <div className="not-owner">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+            nesciunt ullam magni, consequuntur, at accusamus ratione autem
+            corrupti aut quibusdam officia labore tempore hic deserunt sint
+            iste, nihil nostrum alias.
+          </div> */}
         </div>
-        <div className="messenger-body">
-          <div className="messages d-flex">
-            <div className="not-owner">hello am contacting</div>
-            <div className="not-owner">hello am contacting</div>
-            <div className="not-owner">hello am contacting</div>
-            <div className="not-owner">hello am contacting</div>
-            <div className="not-owner">hello am contacting</div>
-            <div className="not-owner">hello am contacting</div>
-            <div className="not-owner">hello am contacting</div>
-            <div className="not-owner">hello am contacting</div>
-            <div className="owner">
-              and am the owner.
-            </div>
-            <div className="not-owner">
-            Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Enim nesciunt ullam magni, consequuntur, at
-              accusamus ratione autem corrupti aut quibusdam officia labore
-              tempore hic deserunt sint iste, nihil nostrum alias.
-            </div>
-          </div>
+        <form onSubmit={(e) => sendMessage(e)}>
           <div className="d-flex send-div">
-            <input type="text" className="form-control" />
-            <button className="contact-send" id="send-button">
+            <input type="text" id="theMessage" className="form-control" />
+            <button className="contact-send" type="submit" id="send-button">
               <i className=" far fa-paper-plane" />
             </button>
           </div>
-        </div>
+        </form>
       </div>
+    </div>
+  );
+  return (
+    <dialog id="contact-diag" className="border border-warning p-2 rounded-2">
+      {user ? messenger : <AuthPage />}
     </dialog>
   );
-  return messenger;
 }
--- src/firebase-comps/firebase-exp.js
+++ src/firebase-comps/firebase-exp.js
@@ -1,9 +1,104 @@
-import React from 'react'
-// import import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-function FirebaseExp() {
+import React from "react";
+import { getAuth, signInWithEmailAndPassword,createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyD7Pds7gKW7V0cv3rrWYwUgDL0xoso-5Uc",
+  authDomain: "my-portfolio-28414.firebaseapp.com",
+  projectId: "my-portfolio-28414",
+  storageBucket: "my-portfolio-28414.appspot.com",
+  messagingSenderId: "160274144232",
+  appId: "1:160274144232:web:99b684b0fb85cc1a38625d",
+};
 
- 
+function firebaseExp() {}
+export function AuthPage() {
+  const signIn = async (e) => {
+    e.preventDefault();
+
+    await signInWithEmailAndPassword(
+      auth,
+      document.getElementById("email").value,
+      document.getElementById("password").value
+    ).catch(async (err) => {
+      if (err.code === "auth/user-not-found") {
+        await createUserWithEmailAndPassword(
+          auth,
+          document.getElementById("email").value,
+          document.getElementById("password").value
+        ).catch((err) => {
+          if (err.code === "auth/weak-password") {
+            alert("password must be greater than 6 characters");
+          } else {
+            console.warn(err.code);
+          }
+        });
+      } else if (err.code === "auth/wrong-password") {
+        alert("wrong Password");
+      } else {
+        console.error(err.code);
+      }
+    });
+  };
+  return (
+    <>
+      <div className="diag-header">
+      <h3>Sign In with Email </h3>
+        <button
+          className="diag-close"
+          onClick={() => {
+            document.getElementById("contact-diag").close();
+          }}
+        >
+          X
+        </button>
+      </div>
+      <div className="m-auto">
+        <form onSubmit={(e) => signIn(e)}>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              id="email"
+              required
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              required
+              name="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-check">
+            <input
+              type="checkbox"
+              name="checkbox"
+              className="form-check-input"
+              id="remember"
+            />
+            <label className="form-check-label" htmlFor="remember">
+              Remember me
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary float-right">
+            Login
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
-export const auth=getAuth()
-export default FirebaseExp
+const app = initializeApp(firebaseConfig);
+export const firestore=getFirestore(app);
+export const auth = getAuth(app);
+export const signOuter = () => signOut(auth);
+export default firebaseExp;
